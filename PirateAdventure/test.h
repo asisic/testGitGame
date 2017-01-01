f083a49153041e5ea21453745973a303b17ea3a0
asisic <sisic.adi@gmail.com>
2017-01-01 19:26:46 +0100

    second commit

---
+++ PirateAdventure/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  PirateAdventure
+//
+//  Created by api on 1/1/17.
+//  Copyright © 2017 api. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ PirateAdventure/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  PirateAdventure
+//
+//  Created by api on 1/1/17.
+//  Copyright © 2017 api. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
